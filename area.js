75c53c3eb32583ae38300bcc28aac22bf928cf49
Sen-442b <67971019+Sen-442b@users.noreply.github.com>
2021-08-15 11:43:30 +0530

    error resolved

--- area.js
+++ area.js
@@ -5,7 +5,7 @@ const message =  document.getElementById("message");
 
 submitBtn.addEventListener("click",()=>{
     if(inputs[0].value && inputs[1].value){
-const answer = (Number(inputs[0].value)* Number(inputs[0].value))/2;
+const answer = (Number(inputs[0].value)* Number(inputs[1].value))/2;
 
    message.innerText= `Area of the given triangle is ${answer}`
     }else{
