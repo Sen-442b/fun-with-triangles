9b8a28cd5a1367d0f4cba70a13d92fe65b537c6f
Sen-442b <67971019+Sen-442b@users.noreply.github.com>
2021-08-18 14:47:39 +0530

    fix:bug removed

--- hyp-of-triangle.js
+++ hyp-of-triangle.js
@@ -4,7 +4,7 @@ const message =  document.getElementById("message");
 
 
 submitBtn.addEventListener("click",()=>{
-    if(inputs[0].value && inputs[1].value &&inputs[0].value>1 && inputs[1].value>1){
+    if(inputs[0].value && inputs[1].value &&inputs[0].value>=1 && inputs[1].value>=1){
  const answer=  sumOfSquares(Number(inputs[0].value),Number(inputs[1].value));
    message.innerText= `Hypotenuse of the given triangle is ${Math.sqrt(answer)}`
     }else{
