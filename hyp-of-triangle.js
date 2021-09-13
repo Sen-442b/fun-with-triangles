be8bd2252b8c809262d74b89bb4bafdc59ac470d
Sen-442b <67971019+Sen-442b@users.noreply.github.com>
2021-09-13 19:18:31 +0530

    fix:extra decimal points

--- hyp-of-triangle.js
+++ hyp-of-triangle.js
@@ -6,7 +6,7 @@ const message =  document.getElementById("message");
 submitBtn.addEventListener("click",()=>{
     if(inputs[0].value && inputs[1].value &&inputs[0].value>=1 && inputs[1].value>=1){
  const answer=  sumOfSquares(Number(inputs[0].value),Number(inputs[1].value));
-   message.innerText= `Hypotenuse of the given triangle is ${Math.sqrt(answer)}`
+   message.innerText= `Hypotenuse of the given triangle is ${Math.sqrt(answer).toFixed(2)}`
     }else{
         message.innerText="Enter all the required inputs"
     }
